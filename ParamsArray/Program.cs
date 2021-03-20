cb634e41de0bb0fedac1043e268014191b925e8a
jackmin-id <minjwus@gmail.com>
2021-03-19 20:19:27 -0600

    ch11

---
+++ ParamsArray/Program.cs
@@ -0,0 +1,34 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace ParamsArray
+{
+    class Program
+    {
+        static void DoWork()
+        {
+            Console.WriteLine(Util.Sum(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
+            Console.WriteLine(Util.Sum(2, 4, 6, 8, 10));
+            Console.WriteLine(Util.Sum(10, 9, 8));
+            //Util.Sum();
+            //Util.Sum(null);
+        }
+
+        static void Main()
+        {
+            try
+            {
+                DoWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
